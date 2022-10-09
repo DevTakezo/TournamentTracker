36c0299d0a0ed244d8425d35d12ec7f45ebf7c01
DevTakezo <ppiwowarski89@o2.pl>
2022-10-09 19:26:14 +0200

    first commit

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// Represents one team in matchup.///
+        public TeamModel TeamCompeting { get; set; }
+
+        /// Represents score of this team. ///
+        public double Score { get; set; }
+
+        /// Represents the matchup that this team came from as the winner.///
+
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
